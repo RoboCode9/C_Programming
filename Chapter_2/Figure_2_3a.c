07698b8fad2eaf47bd70901d9fab29b54f09b100
RoboCode9 <ingresshelps@gmail.com>
2024-01-26 10:37:36 -0600

    Figure 2.3a complete!

---
+++ Chapter_2/Figure_2_3a.c
@@ -0,0 +1,12 @@
+/* Fig. 2.3: fig02_03a.c
+printing on one line with two printf statements!*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    printf( "Welcome " );
+    printf( "to C!" );
+
+    return 0;
+}
